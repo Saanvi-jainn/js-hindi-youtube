153160c5a74b3b2d4a1645bcd8e1576ad275c878
Saanvi Jain <jainsaanvi.16@gmail.com>
2024-12-11 01:16:32 +0530

    scopes in js

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -15,3 +15,39 @@ if(true)
 
 console.log("outer:", a); //300
 //console.log(b); //error
+
+//scope in functions:
+
+function one() {
+    const username = "Saanvi"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);
+        
+    }
+   // console.log(website); //error
+    two()
+}
+
+one() // o/p: Saanvi
+
+// ++++++++++++++++++++ interesting ++++++++++++++++++++++++
+
+//case 1:
+
+console.log(addOne(5)); //in this function syntax, the function can be called anywhere before or after function declaration
+
+function addOne(num) {
+    return num + 1
+}
+
+//case 2:
+
+//console.log(addTwo(5)); //cannot be called before function declaration as it results in error. This is because the function is declared inside a variable here.
+
+const addTwo = function(num) {
+    return num + 2
+}
+
+console.log(addTwo(5));

0f0cfcd788ba2c9031f1fe5cf0078f501185fc99
Saanvi Jain <jainsaanvi.16@gmail.com>
2024-12-14 01:01:32 +0530

    if-else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,32 @@
+ //  if
+
+ if(true) {
+    //code gets evaluated
+ }
+
+ if(false) {
+    //code does not gets evaluated
+ }
+
+ // oprators for checking relations: < , > , <= , >= , == , != , === , !==
+
+ if(2 == '2') {
+    console.log("executed"); //executed
+ } //data type doesn't need to be same
+
+ if(2 === '2') {
+    console.log("executed"); //doesn't get executed
+ } //necessary for data type to be same
+
+ // if-else
+
+ if(false) {
+    console.log("not executed");
+ }
+ else (
+    console.log("executed");
+ )
+
+ // if- else if- else: used for checking multiple conditions
+
+ // logical operators: && (AND) , || (OR) , ! (NOT)
